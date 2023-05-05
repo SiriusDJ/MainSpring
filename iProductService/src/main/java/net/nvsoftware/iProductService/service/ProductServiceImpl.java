d40010b3ee11ecf1ab9af4a53bc801753e5491f0
Sirius Zhang <sirius.zhang997@gmail.com>
2023-05-04 23:00:48 -0700

    ProductService createProduct API

--- iProductService/src/main/java/net/nvsoftware/iProductService/service/ProductServiceImpl.java
+++ iProductService/src/main/java/net/nvsoftware/iProductService/service/ProductServiceImpl.java
@@ -1,4 +1,23 @@
 package net.nvsoftware.iProductService.service;
 
-public class ProductServiceImpl {
+import net.nvsoftware.iProductService.entity.ProductEntity;
+import net.nvsoftware.iProductService.model.ProductRequest;
+import net.nvsoftware.iProductService.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ProductServiceImpl implements ProductService{
+    @Autowired
+    private ProductRepository productRepository;
+    @Override
+    public long createProduct(ProductRequest productRequest) {
+        ProductEntity productEntity = ProductEntity.builder()
+                .productName(productRequest.getProductName())
+                .productPrice(productRequest.getProductPrice())
+                .productQuantity(productRequest.getProductQuantity())
+                .build();
+        productRepository.save(productEntity);
+
+        return productEntity.getProductId();
+    }
+
 }
