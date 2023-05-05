c6a9e532b9f0de947a367d1048cf3ae1e09f71d1
Sirius Zhang <sirius.zhang997@gmail.com>
2023-05-04 22:41:21 -0700

    Added ProductEntity

---
+++ iProductService/src/main/java/net/nvsoftware/iProductService/entity/ProductEntity.java
@@ -0,0 +1,8 @@
+package net.nvsoftware.iProductService.entity;
+
+public class ProductEntity {
+    private long productId;
+    private String productName;
+    private long productPrice;
+    private long productQuantity;
+}
