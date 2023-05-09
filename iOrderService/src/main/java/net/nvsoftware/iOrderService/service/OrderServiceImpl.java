891b48ed82b8c1eb0476738c95bc59a10b1c8b5b
Sirius <sirius.zhang997@gmail.com>
2023-05-09 15:07:13 -0700

    OrderService RestCall ProductService getProductId api + PaymentService getPaymentByOrderId api

--- iOrderService/src/main/java/net/nvsoftware/iOrderService/service/OrderServiceImpl.java
+++ iOrderService/src/main/java/net/nvsoftware/iOrderService/service/OrderServiceImpl.java
@@ -10,6 +10,7 @@ import net.nvsoftware.iOrderService.model.PaymentRequest;
 import net.nvsoftware.iOrderService.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.Instant;
 
@@ -21,9 +22,11 @@ public class OrderServiceImpl implements OrderService{
 
     @Autowired
     private ProductServiceFeignClient productServiceFeignClient;
-
     @Autowired
     private PaymentServiceFeignClient paymentServiceFeignClient;
+
+    @Autowired
+    private RestTemplate restTemplate;
     @Override
     public long placeOrder(OrderRequest orderRequest) {
         // Use OrderService(this) to create order entity with order status CREATED, save it to database
@@ -73,12 +76,23 @@ public class OrderServiceImpl implements OrderService{
         log.info("OrderService getOrderDetailByOrderId: start with orderId " + orderId);
         OrderEntity orderEntity = orderRepository.findById(orderId)
                 .orElseThrow(() -> new RuntimeException("OrderService getOrderDetailByOrderId NOT FOUND for: " + orderId));
+
+        log.info("OrderService RestCall ProductService getByProductId " + orderId);
+
+        OrderResponse.ProductResponse productResponse = restTemplate.getForObject(
+                "http://PRODUCT-SERVIXE/products/" + orderEntity.getProductId(),
+                OrderResponse.ProductResponse.class
+        );
         OrderResponse orderResponse = OrderResponse.builder()
                 .orderId(orderEntity.getOrderId())
                 .totalAmount(orderEntity.getTotalAmount())
                 .orderDate(orderEntity.getOrderDate())
                 .orderStatus(orderEntity.getOrderStatus())
+                .productResponse(productResponse)
                 .build();
+
+
+
         log.info("OrderService getOrderDetailByOrderId: done");
         return orderResponse;
     }
--- iPaymentService/src/main/java/net/nvsoftware/iPaymentService/repository/PaymentRepository.java
+++ iPaymentService/src/main/java/net/nvsoftware/iPaymentService/repository/PaymentRepository.java
@@ -1,7 +1,9 @@
 package net.nvsoftware.iPaymentService.repository;
 
 import net.nvsoftware.iPaymentService.entity.PaymentEntity;
+import net.nvsoftware.iPaymentService.model.PaymentRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PaymentRepository extends JpaRepository<PaymentEntity, Long> {
+    PaymentEntity findByOrderId(long orderId);
 }
